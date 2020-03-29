f8f652bc09e1581e2ec35b3be6d51045bb905576
Mark Poepping <mark.poepping@mmpro.de>
2020-03-29 16:10:36 +0200

    fix(App): Prepare repository for AC semantic release | MP
    Cleaned up repository and use ac-semantic-release

---
+++ .acsemver.js
@@ -0,0 +1,9 @@
+module.exports = {
+  repository: {
+    url: 'https://github.com/mmpro/ac-awssecrets'
+  },
+  changelogFile: __dirname + '/CHANGELOG.md',
+  sections: [
+    {name: 'App' }
+  ],
+}
